28172a48ff933d1c2d38b8164c11c9df09616110
Enzo Androw <enzoandrow9@gmail.com>
2026-06-17 21:07:04 -0400

    Allow blob: in script-src CSP for backtesting bundler

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isDev = process.env.NODE_ENV !== "production";
 
 const contentSecurityPolicy = [
   "default-src 'self'",
-  `script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net${isDev ? " 'unsafe-eval'" : ""}`,
+  `script-src 'self' 'unsafe-inline' blob: https://cdn.jsdelivr.net${isDev ? " 'unsafe-eval'" : ""}`,
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "img-src 'self' data: blob:",
   "font-src 'self' data: https://fonts.gstatic.com",
